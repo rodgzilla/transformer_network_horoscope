dc287951b98bdef50882dcb44ff07c115d5bf2e9
Grégory Châtel <chatel.gregory@gmail.com>
2018-04-23 14:17:41 +0200

    Adding greedy_decode.

--- src/model_utils.py
+++ src/model_utils.py
@@ -1,6 +1,9 @@
 import copy
 from utils import clones
+import torch
 import torch.nn as nn
+from torch.autograd import Variable
+from utils import subsequent_mask
 from layers import MultiHeadedAttention
 from layers import PositionwiseFeedForward
 from layers import PositionalEncoding
@@ -64,6 +67,39 @@ def make_model(src_vocab, tgt_vocab, N = 6, d_model = 512,
 
     return model
 
-if __name__ == '__main__':
-    model = make_model(10, 10, 2)
-    print(model)
+def greedy_decode(model, src, src_mask, max_len, start_symbol):
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
+
+    for i in range(max_len - 1):
+        out = model.decode(
+            memory,
+            src_mask,
+            Variable(
+                ys
+            ),
+            Variable(
+                subsequent_mask(
+                    ys.size(
+                        1
+                    )
+                ).type_as(src.data)
+            )
+        )
+        prob = model.generator(
+            out[:, -1]
+        )
+        _, next_word = torch.max(
+            prob,
+            dim = 1
+        )
+        next_word = next_word.data[0]
+        ys = torch.cat(
+            [
+                ys,
+                torch.ones(1, 1).type_as(src.data).fill_(next_word)
+            ],
+            dim = 1
+        )
+
+    return ys
