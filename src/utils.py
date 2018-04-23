abb3cad3821836dc5f8ace3a337f7473a79b3bc9
Grégory Châtel <chatel.gregory@gmail.com>
2018-04-23 13:02:21 +0200

    Adding attention to utils.

--- src/utils.py
+++ src/utils.py
@@ -36,10 +36,34 @@ def subsequent_mask(size):
     """
     Mask out subsequent positions.
     """
-    attn_shape = (1, size, size)
+    attn_shape      = (1, size, size)
     subsequent_mask = np.triu(
         np.ones(attn_shape),
         k = 1
     ).astype('uint8')
 
     return torch.from_numpy(subsequent_mask) == 0
+
+def attention(query, key, value, mask = None, dropout = None):
+    """
+    Compute 'Scaled Dot Product Attention'.
+    """
+    # Size of network connections
+    d_k    = query.size(-1)
+    # Attention scores
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+
+    # If mask is set, we use it to prevent the network attenting
+    # to future positions in the sequence. It is mainly used in the
+    # decoder.
+    if mask is not None:
+        scores = scores.masked_fill(mask == 0, -1e9)
+
+    # We then use the softmax activitation to mainly focus the attention
+    # output on a single value.
+    p_attn = F.softmax(scores, dim = -1)
+
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+
+    return torch.matmul(p_attn, value), p_attn
