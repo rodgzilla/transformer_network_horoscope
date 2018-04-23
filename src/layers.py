2e6e2f7452c6dbc9b2a4c5c3483fd1b166cff292
Grégory Châtel <chatel.gregory@gmail.com>
2018-04-23 13:32:37 +0200

    Adding more classes to the layers file.

--- src/layers.py
+++ src/layers.py
@@ -1,8 +1,11 @@
+import math
 import torch
-import torch.nn
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 from utils import subsequent_mask
+from utils import attention
+from utils import clones
 
 # Whole network classes
 
@@ -62,6 +65,22 @@ class Generator(nn.Module):
             dim = -1
         )
 
+class PositionwiseFeedForward(nn.Module):
+    """
+    Implements FFN equation.
+    """
+    def __init__(self, d_model, d_ff, dropout = 0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1     = nn.Linear(d_model, d_ff)
+        self.w_2     = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.dropout(F.relu(self.w_1(x)))
+        x = self.w_2(x)
+
+        return x
+
 class LayerNorm(nn.Module):
     """
     Construct a layernorm module.
@@ -93,6 +112,40 @@ class SublayerConnection(nn.Module):
         Apply residual connection to any sublayer with the same size.
         """
         return x + self.dropout(sublayer(self.norm(x)))
+
+# Embeddings and positional encoding layers
+
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Embeddings, self).__init__()
+        self.lut     = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
+
+class PositionalEncoding(nn.Module):
+    """
+    Implement the PE function.
+    """
+    def __init__(self, d_model, dropout, max_len = 5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p = dropout)
+        # Compute the positional encodings once in log space.
+        pe           = torch.zeros(max_len, d_model)
+        position     = torch.arange(0, max_len).unsqueeze(1)
+        div_term     = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
+        pe[:, 0::2]  = torch.sin(position * div_term)
+        pe[:, 1::2]  = torch.cos(position * div_term)
+        pe           = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        pe = Variable(self.pe[:, :x.size(1)], requires_grad = False)
+        x  = x + pe
+        x  = self.dropout(x)
+
+        return x
 
 # Encoder classes
 
@@ -199,3 +252,48 @@ class DecoderLayer(nn.Module):
             x,
             self.feed_forward
         )
+
+# Attention layer
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, h, d_model, dropout = 0.1):
+        """
+        Take in model size and number of heads.
+        """
+        super(MultiHeadedAttention, self).__init__()
+        assert d_model % h == 0
+        # We assume d_v always equals d_k
+        self.d_k     = d_model // h
+        self.h       = h
+        self.linears = clones(
+            nn.Linear(d_model, d_model),
+            4
+        )
+        self.attn    = None
+        self.dropout = nn.Dropout(p = dropout)
+
+    def forward(self, query, key, value, mask = None):
+        if mask is not None:
+            # Same mask applied to all h heads.
+            mask = mask.unsqueeze(1)
+
+        nbatches = query.size(0)
+
+        # 1) Do all the linear projections in batch from d_model => h x d_k
+        query, key, value = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+                             for l, x in zip(self.linears, (query, key, value))]
+
+        # 2) Apply attention on all the projected vectors in batch.
+        x, self.attn = attention(
+            query,
+            key,
+            value,
+            mask = mask,
+            dropout = self.dropout
+        )
+
+        # 3) "Concat" using a view and apply a final linear.
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
+        x = self.linears[-1](x)
+
+        return x
